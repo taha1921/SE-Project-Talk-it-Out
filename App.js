baea966b4c83dc51e18f0ae3c99079ca93a4e8c1
taha1921 <taha.red70@gmail.com>
2019-04-14 00:26:21 +0500

    First commit

---
+++ App.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import {
+  AppRegistry,
+} from 'react-native';
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import OpeningScreen from './components/OpeningScreen.js';
+import SignUpAs from './components/SignUpAs.js';
+import HelperEmail from './components/HelperEmail.js'
+import HelperCode from "./components/HelperCode.js";
+import HelperSignUp from './components/HelperSignUp.js';
+
+const RootStack = createStackNavigator(
+  {
+    Opening: {
+      screen: OpeningScreen
+    },
+    SignUpOptions: {
+      screen: SignUpAs
+    },
+    HelperEmailScreen: {
+      screen: HelperEmail
+    },
+    HelperVerification : {
+      screen: HelperCode
+    },
+    Helpersignup : {
+      screen: HelperSignUp
+    },
+  },
+  {
+    initialRouteName: "Opening",
+
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#1a2942"
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        fontWeight: "bold"
+      }
+    }
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class myapp extends Component{
+
+  render() {
+    return (
+        <AppContainer/>
+    );
+  }
+}
+
+AppRegistry.registerComponent("myapp", () => myapp)
