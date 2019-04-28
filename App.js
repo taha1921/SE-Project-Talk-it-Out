9747d367861388abeab6ae2277dd0250d73ae96c
codeghees <40551128+codeghees@users.noreply.github.com>
2019-04-29 04:02:37 +0500

    Add files via upload

--- App.js
+++ App.js
@@ -18,6 +18,12 @@ import LoginModerator from "./components/LoginModerator.js";
 import RF from "react-native-responsive-fontsize";
 import AuthLoadingScreen from "./components/AuthLoadingScreen.js";
 import Chat from "./components/Chat.js";
+import Awareness from "./components/Awareness.js";
+import Therapist from "./components/CandT";
+import OnlineServices from "./components/onlineservices.js";
+import GenAware from "./components/generalawareness.js";
+import SuicideHotlines from "./components/suicidehelplines.js";
+
 
 const AuthStack = createStackNavigator(
   {
@@ -90,7 +96,39 @@ class HomeScreen extends React.Component {
   }
 }
 
-const AppStack = createStackNavigator({ Home: HomeScreen});
+const AppStack = createStackNavigator(
+  {
+    Home: {
+      screen: Awareness
+    },
+    Therapist: {
+      screen: Therapist
+    },
+    OnlineServices: {
+      screen: OnlineServices
+    },
+    GenAware: {
+      screen: GenAware
+    },
+    SuicideHotlines: {
+      screen: SuicideHotlines
+    },
+  },
+  
+  {
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#1a2942"
+      },
+      headerTintColor: "#929eb2",
+      headerTitleStyle: {
+        fontWeight: "normal",
+        fontSize: RF(2.1)
+      }
+    }
+  }
+  );
 // const AppContainer = createAppContainer(AuthStack);
 
 // export default class myapp extends Component{
