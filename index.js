620a557e5ef40570bf23dca5aea0e40c5ee15142
taha1921 <taha.red70@gmail.com>
2019-05-01 15:11:07 +0500

    temporarily commenting out configuration from here

--- index.js
+++ index.js
@@ -2,19 +2,19 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 import * as firebase from "firebase"
 
-var config = {
-    apiKey: "AIzaSyC8S1ImpMV_LohWmUiOAN6JctQPWw2KMZg",
-    authDomain: "talkitout-123.firebaseapp.com",
-    databaseURL: "https://talkitout-123.firebaseio.com",
-    projectId: "talkitout-123",
-    storageBucket: "talkitout-123.appspot.com",
-    messagingSenderId: "1096506240955"
-};
-firebase.initializeApp(config);
+// var config = {
+//     apiKey: "AIzaSyC8S1ImpMV_LohWmUiOAN6JctQPWw2KMZg",
+//     authDomain: "talkitout-123.firebaseapp.com",
+//     databaseURL: "https://talkitout-123.firebaseio.com",
+//     projectId: "talkitout-123",
+//     storageBucket: "talkitout-123.appspot.com",
+//     messagingSenderId: "1096506240955"
+// };
+// firebase.initializeApp(config);
 
 AppRegistry.registerComponent(appName, () => App);
