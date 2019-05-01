635342efec2b8ab83c52d9399b8791d0684110de
taha1921 <taha.red70@gmail.com>
2019-05-01 15:12:19 +0500

    temporary chat screen

--- components/Chat.js
+++ components/Chat.js
@@ -1,24 +1,46 @@
-import React, {Component} from 'react';
-import {AppRegistry} from 'react-native'
-import { GiftedChat } from 'react-native-gifted-chat';
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native'
+import { GiftedChat } from 'react-native-gifted-chat'; // 0.3.0
+
+import Fire from '../Fire.js';
 
 export default class Chat extends Component {
-    // 2.
+
     static navigationOptions = {
-        title: "Chat"
+        title: 'Chat',
     }
-    // 3.
+
     state = {
         messages: [],
     };
 
+    get user() {
+        return {
+            name: Fire.shared.username,
+            _id: Fire.shared.uid,
+        };
+    }
+
     render() {
-        // 4.
         return (
             <GiftedChat
                 messages={this.state.messages}
+                onSend={Fire.shared.send}
+                user={this.user}
             />
         );
     }
+
+    componentDidMount() {
+        Fire.shared.on(message =>
+            this.setState(previousState => ({
+                messages: GiftedChat.append(previousState.messages, message),
+            }))
+        );
+    }
+    componentWillUnmount() {
+        Fire.shared.off();
+    }
 }
+
 AppRegistry.registerComponent("Chat", () => Chat)
